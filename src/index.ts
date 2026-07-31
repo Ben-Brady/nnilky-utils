853b635bc327b12301a4e6d1d3f88ac8a62672e3
Ben Brady <benbrady04.bb@gmail.com>
2026-07-31 22:54:30 +0100

    feat: added entries

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./random.js";
 export * from "./assert.js";
 export * from "./resolve.js";
 export * from "./sleep.js";
+export * from "./entries.js";
 export * from "./animation/index.js";
 export * from "./exotic/index.js";
